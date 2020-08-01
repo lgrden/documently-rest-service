3daa12e20aad32753f2d89007d8e4f4c2d3c7cf9
lgrden <lgrden@gmail.com>
2020-08-02 00:41:37 +0200

    added EnableHttpRequestLogger

--- src/main/java/io/wegetit/documently/DocumentlyMicroservice.java
+++ src/main/java/io/wegetit/documently/DocumentlyMicroservice.java
@@ -1,11 +1,13 @@
 package io.wegetit.documently;
 
+import io.wegetit.sau.logger.EnableHttpRequestLogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource("classpath:application.properties")
 @SpringBootApplication
+@EnableHttpRequestLogger
 public class DocumentlyMicroservice {
 
 	public static void main(String[] args) {
